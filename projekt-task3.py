bdd412803c0a791ec693f58aa76ede6f884e8fee
Dominika <129169321+dominiquelt@users.noreply.github.com>
2023-06-16 18:41:15 +0200

    Create projekt-task3.py
    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .json
    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .yml
    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .xml

---
+++ projekt-task3.py
@@ -0,0 +1,73 @@
+
+import os
+import argparse
+import json
+import xmltodict
+import yaml
+
+parser = argparse.ArgumentParser(description="konwerter plikow")
+
+parser.add_argument('input_f', type=str,)
+parser.add_argument('output_f', type=str)
+
+args = parser.parse_args()
+
+input_f = args.input_f
+output_f = args.output_f
+
+if os.path.isfile(args.input_f):
+    print('conversion begins!')
+else:
+    print(f"requested input: {args.input_f} does not exist")
+    exit(1)
+
+if input_f.endswith(".yml") or input_f.endswith(".yaml"):
+    with open(input_f, 'r') as y:
+        try:
+            data = yaml.safe_load(y)
+        except yaml.YAMLError as e:
+            print("Parsowanie pliku yaml nie powiodlo sie", e)
+            exit(1)
+elif input_f.endswith(".json"):
+    with open(input_f, 'r') as j:
+        try:
+            data = json.load(j)
+        except json.JSONDecodeError as e:
+            print("Parsowanie pliku json nie powiodlo sie", e)
+            exit(1)
+elif input_f.endswith(".xml"):
+    with open(input_f, 'r') as x:
+        try:
+            data = xmltodict.parse(x.read())
+        except xmltodict.ExpatError as e:
+            print("Parsowanie pliku xml nie powiodlo sie", e)
+            exit(1)
+
+def xml_json_write():
+    jsondata=json.dumps(data,indent=4)
+    with open(output_f,"w") as json_f:
+        json_f.write(jsondata)
+
+def yaml_json_write():
+    with open(output_f,"w") as json_f:
+        json.dump(data,json_f, indent=4)
+
+def xml_yaml_write():
+    yamldata = yaml.dump(data, indent=4)
+    with open(output_f, "w") as yml_f:
+        yml_f.write(yamldata)
+
+def json_yaml_write():
+    with open(output_f,"w") as json_f:
+        yaml.dump(data,json_f,indent=4)
+
+def yaml_xml_write():
+    with open(output_f, "w") as xml_f:
+        xmldata=xmltodict.unparse(data,pretty=True)
+        xml_f.write(xmldata)
+
+def json_xml_write():
+    with open(output_f, "w") as xml_f:
+        xmldata = xmltodict.unparse(data, pretty=True)
+        xml_f.write(xmldata)
+        
